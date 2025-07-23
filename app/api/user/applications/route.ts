9e27b8cdc17a9e278f58f72e44e24b2a97edd8ba
the-ayushm <ayushm1247@gmail.com>
2025-07-23 19:02:11 +0530

    Solved vercel error again

--- app/api/user/applications/route.ts
+++ app/api/user/applications/route.ts
@@ -1,4 +1,5 @@
 // app/api/user/applications/route.ts
+export const dynamic = "force-dynamic";
 import { NextResponse } from 'next/server';
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth"; 
